0a7af1d6e6c9b97d375bf032d9a750745021054d
Lazaro <161037627+lazaro00junio@users.noreply.github.com>
2026-05-19 19:41:25 -0300

    adicionei comentários no código

--- 1ºPeríodo/FSPI/cadastro-alunos/config/Database.php
+++ 1ºPeríodo/FSPI/cadastro-alunos/config/Database.php
@@ -3,12 +3,14 @@ class Database
 {
     public static function conectar(): PDO
     {
+        // define o host, o nome do banco de dados, o usuário como root e a senha como vazia.
         $host = 'localhost';
-
         $dbname = 'cadastro_alunos';
         $usuario = 'root';
         $senha = '';
+
         try {
+            //tenta conectar-se ao banco de dados usando as informações das varíaveis.
             $pdo = new PDO(
                 "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
                 $usuario,
@@ -21,6 +23,7 @@ class Database
             );
             return $pdo;
         } catch (PDOException $e) {
+            //caso a conexão falhe, o programa irá exibir esta mensagem.
             die('Erro ao conectar ao banco de dados: ' . $e->getMessage());
         }
     }
