657c445a4d86c8164a9befe9bcd45de700156486
Lazaro <161037627+lazaro00junio@users.noreply.github.com>
2026-05-19 19:43:31 -0300

    comentando

--- 1ºPeríodo/FSPI/cadastro-alunos/app/Repositories/AlunoRepository.php
+++ 1ºPeríodo/FSPI/cadastro-alunos/app/Repositories/AlunoRepository.php
@@ -3,7 +3,9 @@ class AlunoRepository
 {
     public function salvar(array $dados): bool
     {
+        //conecta-se ao banco de dados
         $pdo = Database::conectar();
+        //insere os valores no banco de dados
         $sql = "INSERT INTO alunos (nome, email, curso)
 VALUES (:nome, :email, :curso)";
         $stmt = $pdo->prepare($sql);
