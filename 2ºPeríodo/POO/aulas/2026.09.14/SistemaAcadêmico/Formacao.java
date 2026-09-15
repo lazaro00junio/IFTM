4595563d39bc7513ae9660e2e3401452dc681306
lazaro00junio <lazarojunio02@gmail.com>
2026-09-14 22:37:10 -0300

    Sistema acadêmico incompleto

---
+++ 2ºPeríodo/POO/aulas/2026.09.14/SistemaAcadêmico/Formacao.java
@@ -0,0 +1,12 @@
+public class Formacao {
+    public String nivel;
+    public boolean conluido;
+    public String instituicao;
+    public publicint ano;
+    public Formacao(String nivel,boolean conluido,String instituicao,int ano){
+        this.nivel=nivel;
+        this.conluido=conluido;
+        this.instituicao=instituicao;
+        this.ano=ano;
+    }
+}
---
+++ 2ºPeríodo/POO/aulas/2026.09.14/SistemaAcadêmico/Pessoa.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+    public String nome;
+    public char sexo;
+    public int idade;
+    public Pessoa(String nome, char sexo, int idade) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+    }
+}
