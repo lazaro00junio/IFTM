c318ae9ed467dbc4af0eddf1f68be4318740fa7e
lazaro00junio <lazarojunio02@gmail.com>
2026-09-21 12:53:59 -0300

    Finaliza o sistema acadêmico

--- 2ºPeríodo/POO/aulas/2026.09.14/SistemaAcadêmico/App.java
+++ 2ºPeríodo/POO/aulas/2026.09.14/SistemaAcadêmico/App.java
@@ -1,58 +1,75 @@
 import java.util.Scanner;
+
 public class App {
-    public static Pessoa lePessoa(){
+    public static Pessoa lePessoa() {
         Scanner s = new Scanner(System.in);
-        String nome=s.nextLine();
+        System.out.print("Insira o nome da pessoa: ");
+        String nome = s.nextLine();
+        System.out.print("Insira o sexo da pessoa: ");
         char sexo = s.next().charAt(0);
+        System.out.print("Insira a idade da pessoa: ");
         int idade = s.nextInt();
-        return p(nome,sexo,idade);
+        return new Pessoa(nome, sexo, idade);
     }
 
-    public static Formacao leFormacao(){
+    public static Formacao leFormacao() {
         Scanner s = new Scanner(System.in);
-        String nivel=s.nextLine();
+        System.out.print("Insira o nível da formação da pessoa: ");
+        String nivel = s.nextLine();
+        System.out.print("O " + nivel + " foi completo? (S/N) ");
         char c = s.next().charAt(0);
+        boolean concluido = c == 's' || c == 'S';
+        while (c != 'n' && c != 'N' && !concluido) {
+            System.out.print("Entrada incorreta. Tente novamente: ");
+            c = s.next().charAt(0);
+        }
         s.nextLine();
-        boolean concluido = c=='s' || c=='S'; 
-        String instituicao =s.nextLine();
+        System.out.print("Insira o nome da instituição de ensino: ");
+        String instituicao = s.nextLine();
+        System.out.print("Insira o ano: ");
         int ano = s.nextInt();
-        return f(nivel,concluido,instituicao,ano)
+        return new Formacao(nivel, concluido, instituicao, ano);
     }
 
-    public static String exibePessoa(Pessoa p){
-
+    public static String exibePessoa(Pessoa p) {
+        return "Nome: " + p.nome + "\nSexo: " + p.sexo + "\nIdade: " + p.idade;
     }
 
-    public  static String exibeFormacao(Formacao f){
-        
+    public static String exibeFormacao(Formacao f) {
+        return "Nível: " + f.nivel + "\nConcluído: " + f.concluido + "\nInstituição: " + f.instituicao + "\nAno: "
+                + f.ano;
     }
 
-    public static void main(String[] args){
-        int op=0;
-        Pessoa p=null;
-        Formacao f=null;
-        while (op!=5){
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int op = 0;
+        Pessoa p = null;
+        Formacao f = null;
+        while (op != 5) {
             System.out.println("====================================");
             System.out.println("1- Preencher dados da  pessoa.");
             System.out.println("2- Preencher dados da formacao.");
             System.out.println("3 - Exibir dados da pessoa.");
             System.out.println("4 - Exibir dados da formacao.");
             System.out.println("5 - Sair");
+            System.out.println("====================================");
+            op = s.nextInt();
             switch (op) {
                 case 1:
-                    lePessoa();
+                    p = lePessoa();
                     break;
                 case 2:
-                    leFormacao();
+                    f = leFormacao();
                     break;
                 case 3:
-                    exibePessoa(p);
+                    System.out.println(exibePessoa(p));
                     break;
                 case 4:
-                    exibeFormacao(f);
+                    System.out.println(exibeFormacao(f));
                 default:
                     break;
             }
         }
+        s.close();
     }
 }
--- 2ºPeríodo/POO/aulas/2026.09.14/SistemaAcadêmico/Formacao.java
+++ 2ºPeríodo/POO/aulas/2026.09.14/SistemaAcadêmico/Formacao.java
@@ -1,12 +1,12 @@
 public class Formacao {
     public String nivel;
-    public boolean conluido;
+    public boolean concluido;
     public String instituicao;
     public int ano;
 
     public Formacao(String nivel, boolean conluido, String instituicao, int ano) {
         this.nivel = nivel;
-        this.conluido = conluido;
+        this.concluido = conluido;
         this.instituicao = instituicao;
         this.ano = ano;
     }
